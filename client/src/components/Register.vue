7e7b131ce362b50fdd0dd5ae8ea85ae097e92a49
Gwinyai Chakonda <gtchax@gmail.com>
2017-09-09 18:31:09 +0800

    first endpoint made

---
+++ client/src/components/Register.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>Register</h1>
+      <input type="email" name="email" placeholder="email"/>
+      <input type="password" name="password" placeholder="password" />
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'register',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -3,8 +3,18 @@ const express    = require('express'),
       morgan     = require('morgan'),
       bodyParser = require('body-parser');
 
+
+const port = process.env.PORT || 8081,
+      app  = express();
+
 //Middleawre
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cors());
+
+app.get('/status', (req, res) => {
+    res.send({ message: "hello from the express backend"})
+});
+
+app.listen(port, () => console.log(`App running on port ${port}.....`));
